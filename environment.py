ef1cbae5a391692f6f9ea32bdd8f47d1132ba1de
nekitboy <n.grigoryev@innopolis.ru>
2019-04-08 21:09:29 +0300

    Environment Base

--- environment.py
+++ environment.py
@@ -1,37 +1,75 @@
-class Test():
+import time
 
-    def string_to_hex(string):
-        return bytes_to_hex(string_to_bytes(string))
+class TestSecurity():
 
+    def __init__(self, algo, data):
+        self.time = -1
+        self.algo = algo
+        self.temp_time = 0
+        self.data = data
 
-    def string_to_bytes(string):
+    @staticmethod
+    def string_to_bytes(self, string):
         return string.encode('utf-8')
 
-
-    def bytes_to_hex(data):
+    @staticmethod
+    def bytes_to_hex(self, data):
         return data.hex()
 
+    @staticmethod
+    def string_to_hex(self, string):
+        return self.bytes_to_hex(self.string_to_bytes(string))
 
-    def bytes_to_string(data):
+    @staticmethod
+    def bytes_to_string(self, data):
         return data.decode('utf-8', errors='replace')
 
-
-    def hex_to_string(hex):
+    @staticmethod
+    def hex_to_string(self, hex):
         return bytes.fromhex(hex)
 
+    def start_timer(self):
+        self.temp_time = time.time()
+
+    def stop_timer(self):
+        return time.time() - self.temp_time
 
     def run(self, encrypt, decrypt):
-        key = ('0'*24)
-        data = '�T1N-x��Y2'.encode('utf-8')
-        print('*** DATA')
-        print(data)
 
-        encrypted = encrypt(data=data)
+        results = []
+        for i, cur_data in enumerate(self.data):
 
-        print('*** Encrypting')
-        print(encrypted)
-        print(encrypted.decode('utf-8', errors='replace'))
+            test = dict()
+            test['id'] = i
+            test['algorithm'] = self.algo
 
-        decoded = decrypt(encrypted)
-        print('*** Decrypting')
-        print(decoded)
+            data = cur_data
+            if isinstance(cur_data, bytes):
+                test['data_str'] = self.bytes_to_string(self, cur_data)
+            if isinstance(cur_data, str):
+                test['data_str'] = cur_data
+                data = cur_data.encode('utf-8')
+            test['data'] = data
+
+            self.start_timer()
+
+            encrypted = encrypt(data=data)
+            test['time_to_encrypt'] = self.stop_timer()
+            test['encrypted'] = encrypted
+
+            self.start_timer()
+            decrypted = decrypt(encrypted)
+            test['time_to_decrypr'] = self.stop_timer()
+
+            test['decrypted'] = decrypted
+            test['decrypted_str'] = self.bytes_to_string(self, decrypted)
+
+            results.append(test)
+        return results
+
+
+def serialize(data):
+    if isinstance(data, bytes):
+        return str(data)
+
+    return data
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from des import triple_des
-from environment import Test
+import json
+from environment import TestSecurity, serialize
 
 key = '0' * 24
 triple_des_m = triple_des(key=key, padmode=2)
-t = Test()
-t.run(triple_des_m.encrypt, triple_des_m.decrypt)
+t = TestSecurity('3-DES', ['123', 'Hello World', 'ASDKMASKFMASKLDMASDMKAS'])
+print(json.dumps(t.run(triple_des_m.encrypt, triple_des_m.decrypt), default=serialize, indent=True))

2d93cc44917ded55e0eb2172d8fb2a368d6fa104
nekitboy <n.grigoryev@innopolis.ru>
2019-04-11 15:33:32 +0300

    Environment add memory usage

--- environment.py
+++ environment.py
@@ -1,4 +1,33 @@
 import time
+import copy
+from memory_profiler import LineProfiler, choose_backend, profile
+
+
+
+def profile1(func):
+    def wrapper(*args, **kwargs):
+        mem_usage = 0
+        prof = LineProfiler(backend=choose_backend('psutil'))
+        val = prof(func)(*args, **kwargs)
+
+        for (filename, lines) in prof.code_map.items():
+
+            for (lineno, mem) in lines:
+                inc = 0
+                if mem:
+                    mem_usage += mem[0]
+                #print('Memory: ', mem)
+
+        #show_results(prof, stream=stream, precision=precision)
+        return val, mem_usage
+
+    return wrapper
+
+@profile1
+def empty():
+    return None
+
+temp, null_memory = empty()
 
 class TestSecurity():
 
@@ -7,6 +36,8 @@ class TestSecurity():
         self.algo = algo
         self.temp_time = 0
         self.data = data
+        self.null_memory = null_memory
+
 
     @staticmethod
     def string_to_bytes(self, string):
@@ -34,7 +65,21 @@ class TestSecurity():
     def stop_timer(self):
         return time.time() - self.temp_time
 
-    def run(self, encrypt, decrypt):
+
+    @profile1
+    def encrypt(self, encryptor, data):
+        a = copy.deepcopy(encryptor)
+        d = copy.deepcopy(data)
+        return a(data=d)
+
+    @profile1
+    def decrypt(self, decryptor, data):
+        a = copy.deepcopy(decryptor)
+        d = copy.deepcopy(data)
+        return a(data=d)
+
+
+    def run(self, encryptor, decryptor):
 
         results = []
         for i, cur_data in enumerate(self.data):
@@ -53,13 +98,17 @@ class TestSecurity():
 
             self.start_timer()
 
-            encrypted = encrypt(data=data)
+            encrypted, memory = self.encrypt(encryptor=encryptor, data=data)
+
             test['time_to_encrypt'] = self.stop_timer()
+            test['memory_to_encrypt'] = (memory - self.null_memory) * 1024
             test['encrypted'] = encrypted
 
             self.start_timer()
-            decrypted = decrypt(encrypted)
-            test['time_to_decrypr'] = self.stop_timer()
+            decrypted, memory = self.decrypt(decryptor=decryptor, data=encrypted)
+            test['time_to_decrypt'] = self.stop_timer()
+            test['time'] = test['time_to_encrypt'] + test['time_to_decrypt']
+            test['memory_to_decrypt'] = (memory - self.null_memory) * 1024
 
             test['decrypted'] = decrypted
             test['decrypted_str'] = self.bytes_to_string(self, decrypted)
@@ -73,3 +122,20 @@ def serialize(data):
         return str(data)
 
     return data
+
+
+def calc_avalanche(bytes1, bytes2):
+    """
+    Calculate avalanche effect
+    :param bytes1: bytes object
+    :param bytes2: bytes object
+    :return: (avalanche_diff, avalanche_percent)
+    """
+    if len(bytes1) != len(bytes2):
+        raise Exception("Couldn't calculate avalanche effect of bytes with different lengths")
+
+    diff = 0
+    for byte1, byte2 in zip(bytes1, bytes2):
+        diff += bin(byte1 ^ byte2).count('1')
+
+    return diff, diff/len(bytes1*8)
--- test.py
+++ test.py
@@ -4,5 +4,10 @@ from environment import TestSecurity, serialize
 
 key = '0' * 24
 triple_des_m = triple_des(key=key, padmode=2)
-t = TestSecurity('3-DES', ['123', 'Hello World', 'ASDKMASKFMASKLDMASDMKAS'])
-print(json.dumps(t.run(triple_des_m.encrypt, triple_des_m.decrypt), default=serialize, indent=True))
+t = TestSecurity('3-DES', ['123', 'Hello World', 'ASDKMASKFMASKLDMASDMKAS'*100, '123'])
+
+results = t.run(triple_des_m.encrypt, triple_des_m.decrypt)
+print(json.dumps(results, default=serialize, indent=True))
+
+def encrypt(data):
+    return encrypt(data=data)
